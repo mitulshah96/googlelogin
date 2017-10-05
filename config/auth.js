25c6cf2560fb3b2f2140a8f4f4b3198e8282dc81
mitulshah96 <mitulshah96@gmail.com>
2017-10-05 19:18:17 +0530

    First Commit

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+    
+        'googleAuth' : {
+            'clientID'      : '71928554953-h0j3pai2hjb1ll0cumldcqnvtvkbpono.apps.googleusercontent.com',
+            'clientSecret'  : 'j-Mdrb6QOcBx3Gr_AfUj7duS',
+            'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        }
+    
+    };
+    
